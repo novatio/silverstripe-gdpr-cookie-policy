08175934cbfc40daec766cc6b6e796267f0726a9
Gabrijel Gavranović <gabrijel@gavro.nl>
2018-05-23 15:35:03 +0200

    mobile tweaks

--- javascript/jquery.policy.js
+++ javascript/jquery.policy.js
@@ -28,18 +28,26 @@
             '#cookie_container p { color:' + options.textColor + ' !important; }' +
             '#cookie_container p a { color:' + options.textColor + ' !important; text-decoration: underline !important; }' +
             '.cookie_inner { width: 90%; margin: 0 auto; } .cookie_inner p { margin: 0; padding-top: 4px; } ' +
-            '#setCookie { float: right; padding: 5px 10px; text-decoration: none; background-color: ' + options.btnColor + ' !important; color: ' + options.btnTextColor + ' !important; margin-right: 10px; } ' +
+            '.cookie_inner .inner_content { float: left}' +
+            '.cookie_inner .inner_buttons { float: right; }' +
+            '#setCookie { padding: 5px 10px; text-decoration: none; background-color: ' + options.btnColor + ' !important; color: ' + options.btnTextColor + ' !important; margin-right: 10px; } ' +
             '#setCookie:hover { background-color: ' + options.btnHoverColor + ' !important; color: ' + options.btnHoverTextColor + ' !important; } ' +
-            '#declineCookie { float: right; padding: 5px 10px; text-decoration: underline; ' + (options.declineBtnColor ? ' background-color: ' + options.declineBtnColor + ' !important; ' : '' ) +  'color: ' + options.declineBtnTextColor + ' !important; } ' +
+            '#declineCookie { padding: 5px 10px; text-decoration: underline; ' + (options.declineBtnColor ? ' background-color: ' + options.declineBtnColor + ' !important; ' : '' ) +  'color: ' + options.declineBtnTextColor + ' !important; } ' +
             '#declineCookie:hover { ' + (options.declineBtnHoverColor ? 'background-color: ' + options.declineBtnHoverColor + ' !important; ' : '' ) + (options.declineBtnHoverTextColor ? 'color: ' + options.declineBtnHoverTextColor + ' !important; ' : '' ) + ' }' +
+            '@media screen and (max-width: 767px) {' +
+            '  .cookie_inner .inner_content, .cookie_inner .inner_buttons { float: none; text-align: center; }' +
+            '  .cookie_inner .inner_content { margin-bottom: 10px; }' +
+            '}' +
         '</style>');
 
         // create popup elements
         $('<div id="cookie_container">' +
             '<div class="cookie_inner">' +
-            '   <a id="declineCookie" href="#">' + options.declineBtnText + '</a>' +
-            '   <a id="setCookie" href="#">' + options.btnText + '</a>' +
-                    options.text +
+            '   <div class="inner_content">' + options.text + '</div>' +
+            '   <div class="inner_buttons">' +
+            '       <a id="setCookie" href="#">' + options.btnText + '</a>' +
+            '       <a id="declineCookie" href="#">' + options.declineBtnText + '</a>' +
+            '   </div>' +
             '</div>' +
         '</div>').appendTo(this);
 
